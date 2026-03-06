fd09c93fc851ae5210559bf5d067220693c3aee2
mxngls <Maximilian.e.hoenig@gmail.com>
2026-03-06 16:36:42 +0900

    Fix broken Atom feed

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ args=(
 	_SITE_EXT_TITLE="\"Max's Homepage\""
 	_SITE_EXT_AUTHOR="\"Maximilian Hoenig\""
 	_SITE_EXT_FEED_ID='b8792f75-efae-40fa-a5b6-e12cfb06dfaa'
-	_SITE_EXT_HOST='maxh.site'
+	_SITE_EXT_HOST='seoul.systems'
 	_SITE_EXT_TAG_SCHEME_DATE='2025-03-02'
 )
 
